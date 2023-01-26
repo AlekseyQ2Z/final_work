d6a567e31108b12ca73e30f33bfcc4eb1b72358b
q2z <89261862522@mail.ru>
2023-01-26 19:48:00 +0300

    wrote code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿void FillterOfArray(string[] arrFiller, string[] arrFilled)
+{
+    int temp = 0;
+    for (int i = 0; i < arrFiller.GetLength(0); i++)
+    {
+        if (arrFiller[i].Length <= 3)
+        {
+            arrFilled[temp] = arrFiller[i];
+            temp++;
+        }
+    }
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+
+string[] arrayString = {"hello", "2", "world", ":-)"};
+string[] arrayFinish = {"", "", "", ""};
+FillterOfArray(arrayString, arrayFinish);
+PrintArray(arrayFinish);
